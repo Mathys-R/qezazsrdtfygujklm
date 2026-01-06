3bef275f5b8e09c4abd27c6dd6d13ee04612a23f
Mathys R <mathys.rosinski@gmail.com>
2026-01-06 11:20:30 +0100

    Update logging to hello world

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.12-slim
+ENV PYTHONUNBUFFERED=1
 COPY . /app
 WORKDIR /app
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
+import sys
 
 if __name__ == '__main__':
-    print('Hello from Target App')
+    print('hello world', flush=True)
     while True:
         time.sleep(10)
